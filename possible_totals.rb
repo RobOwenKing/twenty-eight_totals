6435dc307d8c2650f25e48db7f460f5c02427e76
Robert King <robert.o.king@hotmail.co.uk>
2022-03-08 23:49:14 +0000

    feat: It seems to work

--- possible_totals.rb
+++ possible_totals.rb
@@ -1,17 +1,85 @@
-require 'optparse'
+# frozen_string_literal: true
 
-# @options = {}
-
-# OptionParser.new do |opts|
-#   opts.on('-e', '--equations', 'Show sample equation for each valid total') do
-#     @options[:equations] = true
-#   end
-# end.parse!
-
-def map_by_unary_operators(arr)
-  arr.map { |x| [x, "#{x}!"] }
-     .flatten
+# Returns a new array with elements of arr1 not in arr2
+# Compare: [1, 2, 2] - [1, 2] #=> []
+#          subtract([1, 2, 2], [1, 2]) #=> [2]
+def subtract(arr1, arr2)
+  arr1_dup = arr1.dup
+  arr2.each { |x| arr1_dup.slice!(arr1_dup.index(x)) if arr1_dup.include?(x) }
+  arr1_dup
 end
 
-# p map_by_unary_operators(ARGV.map(&:to_i))
-p map_by_unary_operators(ARGV)
+FACTORIALS = {
+  '0': [0, 1],
+  '3': [3, 6, 720],
+  '4': [4, 24],
+  '5': [5, 120],
+  '6': [6, 720],
+  '7': [7, 5_040],
+  '8': [8, 40_320],
+  '9': [9, 362_880],
+  '10': [10, 3_628_800],
+  '11': [11, 39_916_800],
+  '12': [12, 479_001_600],
+  '13': [13, 6_227_020_800],
+  '14': [14, 87_178_291_200],
+  '15': [15, 1_307_674_368_000],
+  '16': [16, 20_922_789_888_000]
+}.freeze
+
+def push_factorials(num)
+  sym = num.to_s.to_sym
+  return FACTORIALS[sym] if FACTORIALS[sym]
+
+  [num]
+end
+
+##
+# @param []
+#
+def map_by_unary_operators(arr)
+  arr.map { |num| push_factorials(num) }.flatten
+end
+
+def compose_by_binary_operators(num1, num2)
+  returnable = [
+    num1 + num2, num1 - num2, num2 - num1, num1 * num2,
+    "#{num1}#{num2}".to_i, "#{num2}#{num1}".to_i
+  ]
+  returnable << num1**num2 if num2 >= 0.2 && num2 <= 5 && num1 <= 50
+  returnable << num2**num1 if num1 >= 0.2 && num1 <= 5 && num2 <= 50
+  returnable << num1.fdiv(num2) if num2 != 0
+  returnable << num2.fdiv(num1) if num1 != 0
+end
+
+def iterate(arr)
+  arr.length > 1 ? find_possible_totals(arr) : arr
+end
+
+def find_possible_totals(arr)
+  breakdowns = []
+  (1..arr.length / 2).each { |i| breakdowns << arr.combination(i).to_a }
+  breakdowns = breakdowns.flatten(1)
+
+  returnable = []
+  breakdowns.each do |bkd|
+    arr1 = iterate(bkd)
+    arr2 = iterate(subtract(arr, bkd))
+
+    arr1.each do |num1|
+      arr2.each { |num2| returnable << compose_by_binary_operators(num1, num2) }
+    end
+  end
+
+  returnable = returnable.flatten.select { |x| !x.nil? && x.real? }
+  map_by_unary_operators(returnable).uniq.sort
+end
+
+def parse_possible_totals(arr)
+  possible_totals = find_possible_totals(arr)
+
+  possible_totals.filter { |x| x.positive? && x <= 28 && x == x.to_i }
+                 .map(&:to_i).uniq
+end
+
+# p parse_possible_totals([9, 9, 10, 11])
---
+++ possible_totals_with_flags.rb
@@ -0,0 +1,134 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+@options = {}
+
+OptionParser.new do |opts|
+  opts.on('-e', '--equations', 'Show sample equation for each valid total') do
+    @options[:equations] = true
+  end
+  opts.on('-c', '--count', 'Show count of valid solutions') do
+    @options[:count] = true
+  end
+end.parse!
+
+# Returns a new array with elements of arr1 not in arr2
+# Compare: [1, 2, 2] - [1, 2] #=> []
+#          subtract([1, 2, 2], [1, 2]) #=> [2]
+def subtract(arr1, arr2)
+  arr1_dup = arr1.dup
+  arr2.each { |x| arr1_dup.slice!(arr1_dup.index(x)) if arr1_dup.include?(x) }
+  arr1_dup
+end
+
+FACTORIALS = {
+  0.0 => 1,
+  4.0 => 24,
+  5.0 => 120,
+  6.0 => 720,
+  7.0 => 5_040,
+  8.0 => 40_320,
+  9.0 => 362_880,
+  10.0 => 3_628_800,
+  11.0 => 39_916_800,
+  12.0 => 479_001_600,
+  13.0 => 6_227_020_800,
+  14.0 => 87_178_291_200,
+  15.0 => 1_307_674_368_000,
+  16.0 => 20_922_789_888_000
+}.freeze
+
+def find_factorials_hash(key, value)
+  return { 3 => value, 6 => "#{value}!", 720 => "#{value}!!" } if key == 3
+  return { key => value, FACTORIALS[key.to_f] => "#{value}!" } if FACTORIALS[key.to_f]
+
+  { key => value }
+end
+
+##
+# @param []
+#
+def map_by_unary_operators(hsh)
+  returnable = {}
+
+  hsh.each_pair do |key, value|
+    returnable = find_factorials_hash(key, value).merge(returnable)
+  end
+
+  returnable
+end
+
+def compose_by_binary_operators(key1, val1, key2, val2)
+  key1 = key1.to_f
+  key2 = key2.to_f
+
+  returnable = {
+    key1 + key2 => "(#{val1}+#{val2})",
+    key1 - key2 => "(#{val1}-#{val2})",
+    key2 - key1 => "(#{val2}-#{val1})",
+    key1 * key2 => "(#{val1}*#{val2})"
+  }
+
+  returnable["#{key1}#{key2}".to_i] = "#{val1}#{val2}" if "#{key1}#{key2}".to_i == "#{val1}#{val2}"
+  returnable["#{key2}#{key1}".to_i] = "#{val2}#{val1}" if "#{key2}#{key1}".to_i == "#{val2}#{val1}"
+
+  #returnable[key1**key2] = "(#{val1}**#{val2})" if key2 >= 0.2 && key2 <= 5 && key1 <= 50
+  #returnable[key2**key1] = "(#{val2}**#{val1})" if key1 >= 0.2 && key1 <= 5 && key2 <= 50
+
+  returnable[key1.fdiv(key2)] = "(#{val1}/#{val2})" if key2 != 0
+  returnable[key2.fdiv(key1)] = "(#{val2}/#{val1})" if key1 != 0
+
+  returnable
+end
+
+def iterate(arr)
+  return find_possible_totals(arr) if arr.length > 1
+
+  hsh = { arr[0] => arr[0] }
+  map_by_unary_operators(hsh)
+end
+
+def find_possible_totals(arr)
+  arr.map!(&:to_i)
+  breakdowns = []
+  (1..arr.length / 2).each { |i| breakdowns << arr.combination(i).to_a }
+  breakdowns = breakdowns.flatten(1)
+
+  returnable = {}
+  breakdowns.each do |bkd|
+    hsh1 = iterate(bkd)
+    hsh2 = iterate(subtract(arr, bkd))
+
+    hsh1.each_pair do |key1, val1|
+      hsh2.each_pair { |key2, val2| returnable = compose_by_binary_operators(key1, val1, key2, val2).merge(returnable) }
+    end
+  end
+
+  map_by_unary_operators(returnable)
+end
+
+def print_equations(possible_totals)
+  (1..28).each do |i|
+    if possible_totals[i]
+      p "#{i}: #{possible_totals[i]}"
+    else
+      p "#{i} not possible"
+    end
+  end
+end
+
+def parse_possible_totals(arr)
+  possible_totals = find_possible_totals(arr)
+
+  possible_totals.select! { |key| key.positive? && key <= 28 && key == key.to_i }
+                 .transform_keys!(&:to_i)
+
+  p "#{possible_totals.length} valid solutions" if @options[:count]
+
+  p possible_totals.keys.sort
+
+  print_equations(possible_totals) if @options[:equations]
+end
+
+parse_possible_totals(ARGV)
